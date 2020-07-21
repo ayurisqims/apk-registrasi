5c60f54180afb9d44b448a4e47c475b0828f3c47
unknown <ayurisqi73@gmail.com>
2020-07-21 12:46:00 +0700

    commit

--- app/src/main/java/com/example/apk_registrasi/MainActivity.java
+++ app/src/main/java/com/example/apk_registrasi/MainActivity.java
@@ -6,10 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -19,6 +22,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,7 +35,8 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText Email, Password;
+    TextInputEditText txtEmail, txtPassword;
+    TextInputLayout layoutEmail, layoutPassword;
     private String URL_LOGIN = "http://192.168.1.6/api/login";
 
 
@@ -38,25 +44,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
 
+    }
+
+    private void init() {
+        layoutEmail = findViewById(R.id.txtLayoutEmail);
+        layoutPassword = findViewById(R.id.txtLayoutPassword);
+        txtEmail = findViewById(R.id.InputEmail);
+        txtPassword = findViewById(R.id.InputPassword);
         Button login = findViewById(R.id.btnLogin);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = Email.getText().toString().trim();
-                String password = Password.getText().toString().trim();
-
-                if (!email.isEmpty() || !password.isEmpty()) {
-                    Login(email, password);
-                } else {
-                    Email.setError("Masukkan Email");
-                    Password.setError("Masukkan Password");
-
-                }
-            }
-        });
-
         Button register = findViewById(R.id.btnRegist);
+
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,59 +64,106 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validate()) {
+                    login();
+                }
+            }
+        });
+
+        txtEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!txtEmail.getText().toString().isEmpty()) {
+                    layoutEmail.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        txtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (txtPassword.getText().toString().length() > 7) {
+                    layoutPassword.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
-    private void Login(final String email, final String password) {
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject Object = new JSONObject(response);
+    private boolean validate() {
+        if (txtEmail.getText().toString().isEmpty()) {
+            layoutEmail.setEnabled(true);
+            layoutEmail.setError("Masukkan Email");
+            return false;
+        }
+        if (txtPassword.getText().toString().length() < 8) {
+            layoutPassword.setEnabled(true);
+            layoutPassword.setError("Masukkan Password 8 Karakter");
+            return false;
+        }
+        return true;
+    }
 
-                            String success = Object.getString("success");
-                            JSONArray jsonArray = Object.getJSONArray("user");
 
-                            if (success.equals("1")) {
-                                for (int i = 0; i < jsonArray.length(); i++) {
-                                    JSONObject object = jsonArray.getJSONObject(i);
+    private void login() {
 
-                                    SharedPreferences userPref = getApplication(). getApplicationContext().getSharedPreferences("user", Email.getContext().MODE_PRIVATE);
-                                    SharedPreferences.Editor editor = userPref.edit();
-                                    editor.putString("token", object.getString("token"));
-                                    editor.putString("email", object.getString("email"));
-                                    editor.apply();
+        StringRequest request = new StringRequest(Request.Method.POST, URL_LOGIN, response -> {
 
-                                    Toast.makeText(MainActivity.this, "Login berhasil" + email, Toast.LENGTH_SHORT).show();
+            try {
+                JSONObject object = new JSONObject(response);
+                if (object.getBoolean("success")) {
+                    JSONObject user = object.getJSONObject("user");
+                    SharedPreferences userPref = this.getApplicationContext().getSharedPreferences("user", this.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = userPref.edit();
+                    editor.putString("token", object.getString("token"));
+                    editor.putString("email", object.getString("email"));
+                    editor.apply();
 
-                                    Intent login = new Intent(MainActivity.this, Data_kel.class);
-                                    startActivity(login);
-                                }
-                            } else {
-                                Toast.makeText(MainActivity.this, "Email dan Password yang dimasukkan salah", Toast.LENGTH_SHORT).show();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            Toast.makeText(MainActivity.this, "Email dan Password salah", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(MainActivity.this, "Error" +error.toString(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Login Berhasil ", Toast.LENGTH_SHORT).show();
                 }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+            error.printStackTrace();
+
         }) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<>();
-                params.put("email", email);
-                params.put("password", password);
+                params.put("email", txtEmail.getText().toString().trim());
+                params.put("password", txtPassword.getText().toString().trim());
                 return params;
             }
+
         };
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
-        }
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
     }
+}
+
+
