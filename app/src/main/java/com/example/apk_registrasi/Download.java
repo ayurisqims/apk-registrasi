b052bab9573ec4286b336d8332454fbe127bc19d
unknown <ayurisqi73@gmail.com>
2020-08-19 13:50:54 +0700

    Fix download

--- app/src/main/java/com/example/apk_registrasi/Download.java
+++ app/src/main/java/com/example/apk_registrasi/Download.java
@@ -1,18 +1,24 @@
 package com.example.apk_registrasi;
 
+import android.Manifest;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.strictmode.DiskWriteViolation;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -29,6 +35,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -37,6 +44,7 @@ import retrofit2.http.Url;
 
 public class Download extends AppCompatActivity {
 
+    private static final int PERMISSION_STORAGE_CODE = 1000;
     DownloadManager downloadManager;
     SharedPreferences preferences;
     private String URL_LOGOUT = "http://192.168.100.174:8/api/logout/";
@@ -49,16 +57,25 @@ public class Download extends AppCompatActivity {
 
         preferences = getApplicationContext().getSharedPreferences("user", Context. MODE_PRIVATE);
 
-        Button button = findViewById(R.id.webProgramming);
-        button.setOnClickListener(new View.OnClickListener() {
+        TextView webProgram = findViewById(R.id.webProgramming);
+        webProgram.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
 
-                Uri uri = Uri.parse("http://informatika.uin-malang.ac.id/wp-content/uploads/2019/04/Modul-prak-Web-programming.pdf");
-                DownloadManager.Request request = new DownloadManager.Request(uri);
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                Long reference = downloadManager.enqueue(request);
+                isExternalStorageWritable();
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_DENIED) {
+                        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permissions, PERMISSION_STORAGE_CODE);
+                        
+                    } else {
+                        startDownload();
+                    }
+                } else {
+                    startDownload();
+                }
 
             }
         });
@@ -139,6 +156,32 @@ public class Download extends AppCompatActivity {
         });
     }
 
+    private void isExternalStorageWritable() {
+
+    }
+
+    private void startDownload() {
+
+        String link = "http://192.168.100.174/data_soal/Web%20Programming.pdf";
+        Uri uri = Uri.parse(link);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI |
+                DownloadManager.Request.NETWORK_MOBILE);
+        request.setTitle("WebProgramming");
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+        try{
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,"WebProgramming.pdf");
+
+            DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+            downloadManager.enqueue(request);
+            Toast.makeText(this, "Download Berhasil!", Toast.LENGTH_SHORT).show();
+        } catch(Exception e) {
+            Toast.makeText(this, "Download Gagal", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -202,6 +245,22 @@ public class Download extends AppCompatActivity {
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_STORAGE_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startDownload();
+                }
+                else {
+                    Toast.makeText(this, "Permintaan Ditolak ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
 
     }
 }
